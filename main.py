63c6d99bf48793f14ec6d75bc97a6e063e4caedc
Vipin  Tripathi <59695863+VIPIN-creator@users.noreply.github.com>
2022-05-01 22:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 from copy import copy,deepcopy
 import helper as h
 import diretion_gene as d
+import extraction as e
+import  numpy as np
+from PIL import Image
 import random
 
 
@@ -12,11 +15,11 @@ img = cv2.imread("hostim.jpg", 0)
 sec_img=cv2.imread("secret2.png",0)
 y_=deepcopy(img)
 x_=deepcopy(sec_img)
-
+# print(x_)
 # converting to pixels
 host=y_.tolist()
 pix_secret=x_.tolist()
-
+# print(pix_secret)
 # converting decimal to binary
 for i in range(len(pix_secret)):
     for j in range(len(pix_secret)):
@@ -25,92 +28,109 @@ for i in range(len(pix_secret)):
 # converting to 8-bit pixels 
 secret=[]
 secret+=h.conversion_8bit(pix_secret)
+# print(len(secret))
 
 # getting the psnr of stego image
-# print(d.return_psnr(pix,temp3))
+# psnr, stego = d.return_psnr(host,secret)
+
 
 #initialize population
 best=-100000
-best_stego = {}
-populations =([[random.randint(0,1) for x in range(22)] for i in range(300)])
-# print(type(populations))
+best_stego1 = []
+best_stego2 = {}
+populations = [[random.randint(0,1) for x in range(22)] for i in range(300)]
+# print((populations))
 parents=[]
-new_populations = []
+best_population = []
 # print(populations)
 
 
 # fitness score calculation ............
 def fitness_score():
-    global populations, best
+    global populations, best, best_stego1, best_stego2, best_population
     fit_value = []
     fit_score = []
+    all_stego = []
     for i in range(300):
-        # chromosome_value = 0
-        # for j in range(21, 0, -1):
-        #     chromosome_value += populations[i][j]*(2 * (21 - j))
-        #         chromosome_value = -1*chromosome_value if populations[i][0]==1 else chromosome_value
-        # print(chromosome_value)
-        # getGene(populations[i])
-        # fit_value.append(d.return_psnr(host,secret, populations[i]))
         f, stego = d.return_psnr(host,secret, populations[i])
         fit_value.append(f)
-        best_stego[f] = stego
+        all_stego.append(list(stego))
+        best_stego2[f] = stego
     # print(fit_value)
-    fit_value, populations = zip(*sorted(zip(fit_value, populations) , reverse = True))
+    fit_value, populations, all_stego = zip(*sorted(zip(fit_value, populations, all_stego) , reverse = True))
     best= fit_value[0]
+    populations = list(populations)
+    best_population = populations[0]
+    best_stego1 = all_stego[0]
+
 
 # fitness_score()
+# print(len(all_stego), type(best_stego1))
+
+# Tournmanet Selection
+def tournament(population):
+    candidate1 = population[random.randint(0, 299)]
+    candidate2 = population[random.randint(0, 299)]
+    while(candidate1 == candidate2):
+        candidate2 = population[random.randint(0, 299)]
+
+    f1, s1 = d.return_psnr(host, secret, candidate1)
+    f2, s2 = d.return_psnr(host, secret, candidate2)
+    if(f1>f2) : return  candidate1
+    return  candidate2
+
 
 def selectparent():
     global parents
+    parents = []
     #global populations , parents
-
-    parents=populations[0:150]
-    # print(type(parents))
-    # print("Parents ", len(parents))
+    for _ in range(150):
+        child = tournament(populations)
+        parents.append(child)
+    # parents = populations[0:150]
+    # print(len(parents))
 # selectparent()
-
-
+#
+# #
 def crossover():
     global parents
 
     cross_point = random.randint(0, 21)
-    # print("cross ", cross_point)
-    # parents = parents + tuple([(parents[0][0:cross_point + 1] + parents[1][cross_point + 1:22])])
     offsprings = []
+    p_crossover = 0.7
     for j in range (75):
-        if(j%2 == 0):
-            offspring1 = (parents[j][0:cross_point + 1] + parents[j+1][cross_point + 1:22])
-            for i in range (6):
-                offspring1[i+16] = random.randint(0, 1)
-            # print(type(offspring1))
-            # parents = parents + tuple([offspring1])
+            if random.random() > p_crossover:
+                offspring1 = parents[2*j]
+                offspring2 = parents[2*j+1]
+            else:
+                offspring1 = parents[2*j][0:cross_point + 1] + parents[2*j+1][cross_point + 1:22]
+                # for i in range (6):
+                #     offspring1[i+16] = random.randint(0, 1)
+                # print(type(offspring1))
+                # parents = parents + tuple([offspring1])
 
-            offspring2 = (parents[j+1][0:cross_point + 1] + parents[j][cross_point + 1:22])
-            for i in range(6):
-                offspring2[i + 16] = random.randint(0, 1)
-        offsprings.append(offspring1)
-        offsprings.append(offspring2)
+                offspring2 = parents[2*j+1][0:cross_point + 1] + parents[2*j][cross_point + 1:22]
+                # for i in range(6):
+                #     offspring2[i + 16] = random.randint(0, 1)
+            offsprings.append(offspring1)
+            offsprings.append(offspring2)
 
 
-    offsprings=tuple(offsprings)
-    # print(len(offsprings),len(parents),len(offspring1))
     parents = parents + offsprings
-
-    # print(len(parents))
-
+    # print(len(parents), len(offsprings))
 
 # crossover()
 #
 def mutation() :
+    mutation_rate = 0.04
     global populations, parents
-    mute = random.randint(0,100)
-    if mute == 4 :
+
+    if random.random() < mutation_rate :
         x=random.randint(0,299)
         y = random.randint(0,21)
         parents[x][y] = 1-parents[x][y]
     populations = parents
-    # print(populations)
+    # print(len(populations))
 # mutation()
 #
 # # [1,0,1,1,1,1,1,1,1,0,0,1,0,0,1,1,1,0,0,0,1,0]
@@ -123,10 +143,37 @@ for i in range(10) :
 print("best score :")
 print(best)
 print("sequence........")
-print(populations[0])
-print("stego.........")
-print(len(best_stego[best]))
+print(best_population)
+# print("stego.........")
+# print(best_stego1)
+print(".....................")
+# print(np.array(best_stego2[best]))
 
-# Extraction
+# if best_stego2[best] != best_stego1:
+#     print(type(best_stego2[best]), type(best_stego1))
 
+# # Extraction
+new_secret = e.return_secret(best_stego2[best], best_population)
+# if h.getValue(new_secret[0:8]) == h.getValue(new_secret[8*128: 8*128+8]):
+#     print("YES")
+# print(h.getValue(new_secret[0:8]), h.getValue(new_secret[8*128: 8*128+8]))
+real_secret = np.array(h.convert_decimal(new_secret))
+print(x_)
+print(real_secret)
+print(np.array_equal(x_,real_secret))
+array = np.array(real_secret, dtype=np.uint8)
+new_image = Image.fromarray(array)
+new_image.save("extracted_secret.png")
+
+array = np.array(best_stego1, dtype=np.uint8)
+new_image = Image.fromarray(array)
+new_image.save("stego.png")
+
+# from matplotlib import pyplot as plt
+# best_stego1 = np.array(best_stego1, dtype=np.uint8)
+# histr = cv2.calcHist([best_stego1], [0], None, [256], [0, 256])
+# #
+# # # show the plotting graph of an image
+# plt.plot(histr)
+# plt.show()
 
